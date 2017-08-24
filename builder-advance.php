99d5031c739884c40d420f8a04e602a27af5cef1
Daniel Maldonado <daniel@danielmdesigns.com>
2017-08-24 13:45:26 -0500

    :computer: Builder Advance

---
+++ builder-advance.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html >
+<head>
+<meta charset="UTF-8">
+<title>eCommission - Builder Advance</title>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400, 400i,700" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/css/lp/builder-advance.css">
+</head>
+
+<body>
+  <header>
+  <div class="header_container">
+    <img class="brand" src="/img/landing_pages/logo-white.png" alt="eCommission Financial Services">
+    <p><strong>Builder&nbsp;Advance</strong> provides commission advances to real estate agents as an incentive to show and sell your properties. As soon as a home is under contract, the buyer’s agent can receive up to $4,000 of their net commission within 24 hours. Best of all,
+      you can enjoy all the benefits of offering <strong>Builder&nbsp;Advance</strong> with no administrative responsibility, risk or use of your own capital!</p>
+  </div>
+</header>
+
+<main>
+  <article>
+    <div class="article_icon">
+      <img src="/img/landing_pages/icons/icon-howitworks.png" style="width: 50px" alt="">
+    </div>
+    <div class="article-content">
+      <h2>How It Works</h2>
+      <p>Builders typically offer buyer’s agents a traditional 3% commission. Under the program, agents would be given a choice: Receive 3% but wait until closing to get paid. Or choose Builder Advance and receive 2.5% but have their commission wired to
+        their bank within a few hours.*<br><br> If choosing Builder Advance, the Builder uses the .5% reduction to offset the cost of the advance which is a flat fee of 25%, and due only when the sale closes. Here’s an example:
+        <br><br><br>
+        <img id="desktop" src="/img/landing_pages/BuilderAdvance_Table.png" alt="Desktop Pricing Table">
+        <img id="mobile" src="/img/landing_pages/BuilderAdvance_Table_Stacked.png" alt="Mobile Pricing Table">
+      </p>
+    </div>
+  </article>
+
+  <article>
+    <div class="article_icon">
+      <img src="/img/landing_pages/icons/icon-sellit.png" alt="">
+    </div>
+    <div class="article-content">
+      <h2>Sell It</h2>
+      <p>Add Builder Advance to your current marketing plan today, and replace the need to offer higher commission structures or elaborate sales promotions. Give agents what they value most: <em>immediate payment of the commissions they’ve earned!</em>
+      </p>
+    </div>
+  </article>
+
+  <article class="article-centered">
+    <div class="article-content">
+      <h2>Interested?</h2>
+      <p>Give us feedback! Please help us bring Builder Advance to market by answering the following 5 question survey.
+      </p>
+      <a href="https://www.surveymonkey.com/r/DWQWZQ8" class="btn" target="_blank">Take Survey</a>
+    </div>
+  </article>
+
+  <p class="copyright"><small>&#169;</small>2017 eCommission Financial Services, Inc.</p>
+
+</main>
+  
+  
+</body>
+<!-- Maropost Default Tracking Start-->
+<script async src='https://content.maropost.com/uploads/1158/websites/1/4121fad7e9735abb4e4617110e6690df4dfecd2f-v4.js' type='text/javascript'></script>
+<!-- Maropost Default Tracking End-->
+</html>
